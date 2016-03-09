565dcc7ed899ae905f4ceb066c39ff59f231b662
Gary Traffanstedt <blimey85@gmail.com>
2016-03-09 02:04:23 -0500

    dkddk

--- lib/active_nutrition/migrations/00000000001_migrations_table.rb
+++ lib/active_nutrition/migrations/00000000001_migrations_table.rb
@@ -4,10 +4,10 @@ module ActiveNutrition
   module Migrations
     class MigrationsTable < ActiveRecord::Migration
       def self.up
-        # create_table :active_nutrition_migrations do |t|
-        #   t.integer :sequence_no
-        #   t.timestamps
-        # end
+        create_table :active_nutrition_migrations do |t|
+          t.integer :sequence_no
+          t.timestamps
+        end
       end
 
       def self.down
